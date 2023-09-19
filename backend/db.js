44374634863767f350e1645b9c1673109b66d60a
chelawatak <chelawatak@gmail.com>
2023-09-19 15:33:35 +0530

    backend correction

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,10 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb://127.0.0.1:27017/memovibe"
+// const mongoURI="mongodb://127.0.0.1:27017/memovibe"
+const mongoURI=process.env.mongoURI;
 
 
 const connectToMongo=()=>{
+
     mongoose.connect(mongoURI);
     console.log("connected to mongo successfully");
 }
--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,12 @@ const express = require('express')
 var cors = require('cors')
 connectToMongo();
 
+
+const dotenv = require("dotenv")
+dotenv.config();
+
 const app = express()
-const port = 5000
+const port = process.env.PORT||5000;
 
 app.use(cors())
 app.use(express.json())
