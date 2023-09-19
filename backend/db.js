5d8a5cc0e8da1150c9c9aa7dff08259f34da2372
chelawatak <chelawatak@gmail.com>
2023-09-19 16:12:55 +0530

    second change in backend

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,26 @@
 const mongoose=require('mongoose');
 // const mongoURI="mongodb://127.0.0.1:27017/memovibe"
-const mongoURI=process.env.mongoURI;
+const MONGODB_URL='mongodb+srv://chelawatak:NHmD7xlzaEfaqzdH@cluster0.2duuuph.mongodb.net/memovibe';
+require("dotenv").config();
 
 
 const connectToMongo=()=>{
 
-    mongoose.connect(mongoURI);
-    console.log("connected to mongo successfully");
+    mongoose.connect(MONGODB_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    })
+
+    .then(() => console.log("DB Connected Successfully"))
+    .catch( (error) => {
+        console.log("DB Connection Failed");
+        console.error(error);
+        process.exit(1);
+    } )
+    // console.log(mongoURI);
+    // console.log("connected to mongo successfully");
 }
+
+
 
 module.exports = connectToMongo;
