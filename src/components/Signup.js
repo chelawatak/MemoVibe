cf0713d0c404d01826fca0b77f8dfd1172282d0b
chelawatak <chelawatak@gmail.com>
2024-02-09 22:27:02 +0530

    signup login linked

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const Signup = (props) => {
@@ -145,6 +146,10 @@ const Signup = (props) => {
         <button disabled={change} type="submit" className="btn logout-btn my-3">
           Submit
         </button>
+
+        <p>
+          Already have an account? <Link to="/login">login</Link>
+        </p>
       </form>
     </div>
   );
